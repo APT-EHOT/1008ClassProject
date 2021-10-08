847010093e0f787637daf89c619f3bf66ac7f7f1
Artemiy Matchin <art16ws@gmail.com>
2021-10-08 19:05:04 +0300

    Initial commit

---
+++ app/src/main/java/com/artemiymatchin/a1008classproject/MainActivity.java
@@ -0,0 +1,34 @@
+package com.artemiymatchin.a1008classproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    TextView textView;
+    private int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+                textView.setText(Integer.toString(counter));
+            }
+        });
+
+    }
+}
